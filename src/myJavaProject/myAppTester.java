75817c9212ed16b0e7d8f1e560e8fae0a8769e0c
humaimidi-m1234 <66305612+humaimidi-m1234@users.noreply.github.com>
2020-06-28 02:30:43 -0700

    Update myAppTester.java

--- src/myJavaProject/myAppTester.java
+++ src/myJavaProject/myAppTester.java
@@ -17,14 +17,7 @@ public class myAppTester {
 	  reverseMethod(n);
 	  System.out.println();
 
-	  wordCount("Reverse of the input number is:");
-
-	  System.out.println("Hello from James 300319276");
-	  System.out.println("Dai-Dung Nguyen CSIS 3275 - 002");
-
-
-	  // add a line below to call your myTestMethod from here  then remove the block comment symbols  
-	  myTestMethod();
+	 
 	}
 	
 	//This is a method to reverse the number by recursion
@@ -39,25 +32,12 @@ public class myAppTester {
 	       }
 	   }
 	
-	//this is a new method
-		public static void WordCount(String sentenceIs) {
-		String eachWordIs=sentenceIs.split(' ');
-		System.out.println(eachWordIs);
-		for(int x=1;eachWordIs>0;x++) {
-		
-			return x; 
 			
-		}
-	
-	}
 
 
 	   
 	   //    add your test method in this space then remove the block comment symbols
-	       public static void myTestMethod(){
-
-            System.out.println("This is my test method. ");
-
+	       
 		   }
 	 
 	   
